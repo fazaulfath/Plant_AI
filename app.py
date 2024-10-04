7e25f1019e1c7ff9db53d2120ce35b5e17158267
Faza Ulfath <149148613+fazaulfath@users.noreply.github.com>
2024-10-04 23:40:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,26 +5,29 @@ import utils
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def home():
     return render_template('index.html')
-
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         try:
             file = request.files['file']
+            if not file:
+                return render_template('index.html', status=400, res="No file uploaded.")
+            
             img = file.read()
             prediction = predict_image(img)
             print(prediction)
             res = Markup(utils.disease_dic[prediction])
             return render_template('display.html', status=200, result=res)
-        except:
-            pass
-    return render_template('index.html', status=500, res="Internal Server Error")
-
+        
+        except Exception as e:
+            print(f"Error during prediction: {e}")  # Log the error
+            return render_template('index.html', status=500, res="Internal Server Error")
+    
+    return render_template('index.html')
 
 # if __name__ == "__main__":
 #     app.run(debug=True)
